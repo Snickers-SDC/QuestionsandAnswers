47dc1e197c7f33b516d860ea17e78f5f37961755
Felipe Erazo <felipe.erazoangarita@gmail.com>
2021-06-27 00:45:43 -0500

    Creatted all Queris RESTFUL for MongoDB

--- questions_answers/mongo_database/index.js
+++ questions_answers/mongo_database/index.js
@@ -11,31 +11,137 @@ db.once('open', function() {
   console.log('Mongo connected!')
 });
 
-const answersSchema = Schema({
+const answerSchema = Schema({
+  question_id: Number,
   body: String,
-  name:String,
-  email: String,
-  helpfulness: Number,
-  reported: Boolean,
+  answerer_name:String,
+  answerer_email: String,
+  reported: { type: Number, default: 0 },
+  helpful: { type: Number, default: 0 },
   photos: [{
+    id: Number,
     url: String
   }]
-})
+}, { timestamps: true })
 
-const Answers = mongoose.model('Question', answersSchema);
+const Answer = mongoose.model('Answerscombine', answerSchema, 'answerscombines');
 
 const questionSchema = Schema({
+  product_id: Number,
   body: String,
-  name: String,
-  email: String,
-  product_id: String,
-  helpfulness: Number,
-  reported: Boolean,
-  answers: { type: mongoose.ObjectId, ref: Answers }
+  asker_name: String,
+  asker_email: String,
+  reported: { type: Number, default: 0 },
+  helpful: { type: Number, default: 0 },
 }, { timestamps: true });
 
 
 
-const Question = mongoose.model('Question', questionSchema);
+const Question = mongoose.model('Question', questionSchema, 'questions');
 
 // dev start
+const getQuestionsById = (id, callback) => {
+  Question.find({product_id: id, reported: 0}, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const getAnswersByQuestionId = (id, callback) => {
+  Answer.find({question_id: id, reported: 0}, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const addNewQuestion = (quest, callback) => {
+  const newQuestion = new Question ({
+    product_id: quest.product_id,
+    body: quest.body,
+    asker_name: quest.asker_name,
+    asker_email: quest.asker_email,
+  });
+
+  Question.save(newQuestion, (err, data) => {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const addNewAnswer = (answ, callback) => {
+  const newAnswer = new Answer ({
+    question_id: answ.question_id,
+    body: answ.body,
+    answerer_name: answ.answerer_name,
+    answerer_email: answ.answerer_email,
+    photos: answ.photos,
+  });
+
+  Answer.save(newAnswer, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const markQuestionHelpful = (id, callback) => {
+  Question.findOneAndUpdate({id: id}, { $inc: {helpful: 1}}, {new: true}, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const reportQuestion = (id, callback) => {
+  Question.findOneAndUpdate({id: id}, { $inc: {reported: 1}}, {new: true}, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const markAnswerHelpful = (id, callback) => {
+  Answer.findOneAndUpdate({id: id}, { $inc: {helpful: 1}}, {new: true}, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+const reportAnswer = (id, callback) => {
+  Answer.findOneAndUpdate({id: id}, { $inc: {reported: 1}}, {new: true}, (err, data) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+module.export = {
+  getQuestionsById,
+  getAnswersByQuestionId,
+  addNewQuestion,
+  addNewAnswer,
+  markQuestionHelpful,
+  reportQuestion,
+  markAnswerHelpful,
+  reportAnswer,
+}
